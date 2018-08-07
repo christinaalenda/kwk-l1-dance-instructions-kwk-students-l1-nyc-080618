d38ce957e76355ba981219e5188353034bfea7d5
christinaalenda <clenda041@gmail.com>
2018-08-07 13:59:13 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -3,34 +3,34 @@ def starting_stance
 end
   
 def base_footwork  
-  puts "lift right foot"
-  puts "return right foor to the ground"
+  puts "Lift right foot"
+  puts "Return right foor to the ground"
   #finish step 3
 end
 
 def skip_step
-  puts "lower #{starting_foot} to the ground"
-  puts "bounce #{starting_foot} back up slightly, kicking it a few inches back"
-  puts "briefly raise the opposite foot, then the #{starting_foot} , twice more!"
+  puts "Lower #{starting_foot} to the ground"
+  puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
+  puts "Briefly raise the opposite foot, then the #{starting_foot} , twice more!"
   skip_step("left foot")
 end
 
 def bob_the_reins
-  puts "holding your arms out in front of you, straight and at chest level"
-  puts "cross your right wrist over your left and hold them together"
-  puts "lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
-  puts "do this 7 more times!"
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+  puts "Do this 7 more times!"
 end
 
 def lasso
   puts "start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left"
-  puts "lift right arm up and point right elbow diagonally to the right"
-  puts "raise right forearm so that it points straight up, and whip it in small circls to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+  puts "Lift right arm up and point right elbow diagonally to the right"
+  puts "Raise right forearm so that it points straight up, and whip it in small circls to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
   puts "do this 7 more times!"
 end
 
 def bust_a_move
-  puts "be cool and be snazzy ;)"
+  puts "Be cool and be snazzy ;)"
 end
 
 def gangman_style
