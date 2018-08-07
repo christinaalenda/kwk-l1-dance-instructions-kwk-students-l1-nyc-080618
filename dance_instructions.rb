98fef4a5443c510f5221ea10c0a94ce79fb30831
christinaalenda <clenda041@gmail.com>
2018-08-07 14:01:08 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -5,10 +5,10 @@ end
 def base_footwork  
   puts "Lift right foot"
   puts "Return right foor to the ground"
-  #finish step 3
+  skip_step("left foot")
 end
 
-def skip_step
+def skip_step(starting_foot)
   puts "Lower #{starting_foot} to the ground"
   puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
   puts "Briefly raise the opposite foot, then the #{starting_foot} , twice more!"
